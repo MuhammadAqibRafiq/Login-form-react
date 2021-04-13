655a6ea7b589b53e9aa9c2d8f3ceee7cfc2cd0f3
Muhammadaqibrafiq <aqibrafiq001@outlook.com>
2021-04-13 22:55:11 +0500

    'first'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+
+const App = () => {
+  const [fullname,setFullname]=useState({fname:'',lname:''});
+
+
+  const inputEvent = (e) => {
+    // setName(e.target.value);
+    
+    const value = e.target.value;
+    const name = e.target.name;
+
+    setFullname ((prevalue) => {
+      if(name === 'fname'){
+        return {
+          fname:value,
+          lname:prevalue.lname
+        }
+      }
+      else if(name === 'lname') {
+        return {
+          fname:prevalue.fname,
+          lname:value,
+        }
+      }
+    });
+  
+  };
+
+  const onSubmits = (e) => {
+    e.preventDefault();
+    alert("foam submitted");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main'>
+
+      <foam onSubmit={onSubmits}>
+
+    <div>
+      <h1>FOAM {fullname.fname} {fullname.lname}</h1>
+     
+        <input
+        type = "email"
+        placeholder= "enter email"
+        name="fname"
+        onChange={inputEvent}
+        />
+        <br/>
+        <br/>
+        <input
+        type="password"
+        placeholder="enter pass.."
+        name="lname"
+        onChange={inputEvent}
+        />
+        <br/>
+        <br/>
+      <button type="submit"> submit  </button>
+    </div>
+    </foam>
     </div>
   );
 }
